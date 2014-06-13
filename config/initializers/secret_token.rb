90164ec773d14ef086aa37c259cf8b5d43a301d2
hesler <piotr.kochowicz@m2a.pl>
2014-06-14 00:25:07 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Savie::Application.config.secret_token = '86814ab08802c7edc9ee4abc8b5da2dfb4ea32aa922e751221775ff7c59bd4203c088dbc0ad2d72e5de19076c2a9806db15986075d791d56c603479d64fd86b4'
